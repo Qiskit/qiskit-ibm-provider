7b87a157b28b22193be1e859807abeb84046c4f2
Kevin Tian <kevin.tian@ibm.com>
2021-09-08 09:43:39 -0400

    revert job_widget & test_jupyter

--- qiskit_ibm/jupyter/jobs_widget.py
+++ qiskit_ibm/jupyter/jobs_widget.py
@@ -115,8 +115,7 @@ def _job_summary(backend: Union[IBMQBackend, FakeBackend]) -> PlotlyWidget:
     now = datetime.datetime.now()
     past_year_date = now - datetime.timedelta(days=365)
     date_filter = {'creationDate': {'gt': past_year_date.isoformat()}}
-    provider = backend.provider()
-    jobs = provider.backend.jobs(limit=None, db_filter=date_filter)
+    jobs = backend.jobs(limit=None, db_filter=date_filter)
 
     num_jobs = len(jobs)
     main_str = "<b>Total Jobs</b><br>{}".format(num_jobs)
--- test/ibmq/test_jupyter.py
+++ test/ibmq/test_jupyter.py
@@ -82,8 +82,8 @@ class TestBackendInfo(IBMQTestCase):
             kwargs['start_datetime'] = datetime.now() - timedelta(days=7)
             return original_backend_jobs(**kwargs)
         for backend in self.backends:
-            provider = backend.provider()
             with self.subTest(backend=backend):
+                provider = backend.provider()
                 original_backend_jobs = provider.backend.jobs
                 provider.backend.jobs = _limit_jobs
                 jobs_tab(backend)
