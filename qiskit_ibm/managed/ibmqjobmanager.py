780dcb247f3603ae69ba0f8535c1a0334de96bee
Kevin Tian <kt474@cornell.edu>
2021-09-12 16:09:15 -0400

    Update qiskit_ibm/managed/ibmqjobmanager.py
    Co-authored-by: Rathish Cholarajan <rathishc24@gmail.com>

--- qiskit_ibm/managed/ibmqjobmanager.py
+++ qiskit_ibm/managed/ibmqjobmanager.py
@@ -109,7 +109,10 @@ class IBMQJobManager:
                 the backend.
                 If the specified value is greater the maximum allowed by the
                 backend, the default is used.
-            job_tags: Tags to be assigned to the jobs.
+            job_tags: Tags to be assigned to the jobs. The tags can subsequently be used
+                   as a filter in the 
+                   :meth:`IBMQBackendService.jobs()<qiskit_ibm.ibmqbackendservice.IBMQBackendService.jobs>` 
+                   method.
             run_config: Configuration of the runtime environment. Some
                 examples of these configuration parameters include:
                 ``qobj_id``, ``qobj_header``, ``shots``, ``memory``,
