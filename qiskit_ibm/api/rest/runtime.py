419258c3a91b22e4e6e8b4f04c516b0a4f63b0af
Renier Morales <renierm@us.ibm.com>
2021-10-08 00:06:43 -0500

    The qiskit runtime api needs to update the program upload body request
    This needs to change in the program upload body request in order to
    meet the IBM Cloud API guidance.

--- qiskit_ibm/api/rest/runtime.py
+++ qiskit_ibm/api/rest/runtime.py
@@ -145,7 +145,7 @@ class Runtime(RestAdapterBase):
         """
         url = self.get_url('jobs')
         payload = {
-            'programId': program_id,
+            'program_id': program_id,
             'hub': hub,
             'group': group,
             'project': project,
