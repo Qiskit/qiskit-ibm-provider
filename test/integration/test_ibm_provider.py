0980015970daf5c973d78d1f80c0040142a7e30d
Kevin Tian <kevin.tian@ibm.com>
2022-04-18 11:52:12 -0400

    update token test

--- test/integration/test_ibm_provider.py
+++ test/integration/test_ibm_provider.py
@@ -52,12 +52,15 @@ class TestIBMProviderEnableAccount(IBMTestCase):
         super().setUpClass()
         cls.dependencies = dependencies
 
-    # TODO enable test
-    @skip("Fails on staging because token is invalid for URL")
     def test_provider_init_token(self):
         """Test initializing IBMProvider with only API token."""
         # pylint: disable=unused-argument
-        provider = IBMProvider(token=self.dependencies.token)
+        if self.dependencies.url == AUTH_URL:
+            provider = IBMProvider(token=self.dependencies.token)
+        else:
+            provider = IBMProvider(
+                token=self.dependencies.token, url=self.dependencies.url
+            )
         self.assertIsInstance(provider, IBMProvider)
         self.assertEqual(provider._client_params.token, self.dependencies.token)
 
