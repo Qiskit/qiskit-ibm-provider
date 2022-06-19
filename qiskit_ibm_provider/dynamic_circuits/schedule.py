a518281ce7a863da3572825753145b9d3c165995
Thomas Alexander <thomas.a.alexander@ibm.com>
2022-06-19 16:04:17 -0300

    Refactor nodes to no longer return. Remove special handling for classical bits.

--- qiskit_ibm_provider/dynamic_circuits/schedule.py
+++ qiskit_ibm_provider/dynamic_circuits/schedule.py
@@ -96,7 +96,7 @@ class DynamicCircuitScheduleAnalysis(BaseScheduler):
         # t0: start time of instruction
         # t1: end time of instruction
         if isinstance(node.op, self.CONDITIONAL_SUPPORTED):
-            t0, t1 = self._visit_conditional_node(node)
+            self._visit_conditional_node(node)
         else:
             if node.op.condition_bits:
                 raise TranspilerError(
@@ -104,20 +104,25 @@ class DynamicCircuitScheduleAnalysis(BaseScheduler):
                 )
 
             if isinstance(node.op, Measure):
-                t0, t1 = self._visit_measure(node)
+                self._visit_measure(node)
             else:
-                t0, t1 = self._visit_generic(node)
-
-        for bit in node.qargs:
-            self._idle_after[bit] = t1
-
-        self._node_start_time[node] = t0
+                self._visit_generic(node)
 
     def _visit_conditional_node(self, node):
-        op_duration = self._get_node_duration(node)
+        """Handling case of a conditional execution.
 
-        t0q = max(self._idle_after[q] for q in node.qargs)
+        Conditional execution durations are currently non-deterministic. as we do not know
+        the time it will take to begin executing the block. We do however know when the
+        duration of the block contents execution (provided it does not also contain
+        conditional executions).
+
+        TODO: Update for support of general control-flow, not just single conditional operations.
+        """
         if node.op.condition_bits:
+            # Special processing required to resolve conditional scheduling dependencies
+            op_duration = self._get_node_duration(node)
+
+            t0q = max(self._idle_after[q] for q in node.qargs)
             # conditional is bit tricky due to conditional_latency
             t0c = max(self._idle_after[bit] for bit in node.op.condition_bits)
             if t0q > t0c:
@@ -142,47 +147,32 @@ class DynamicCircuitScheduleAnalysis(BaseScheduler):
                 self._idle_after[bit] = t1c
             # It starts after register read access
             t0 = max(t0q, t1c)
-        else:
-            t0 = t0q
-            t1 = t0 + op_duration
 
-        t1 = t0 + op_duration
-        return t0, t1
+            t1 = t0 + op_duration
+            self._update_idles(node, t0, t1)
+        else:
+            # Fall through to generic case if not conditional
+            self._visit_generic(node)
 
     def _visit_measure(self, node):
+        """Visit a measurement node.
+
+        Measurement currently triggers the end of a pulse block in IBM dynamic circuits hardware.
+        This means that it is possible to schedule *up to* a measurement (and during its pulses)
+        but the measurement will be followed by a period of indeterminism.
+        All measurements on disjoing qubits will be collected on the same qubits to be run simulataneously.
+
+        """
         op_duration = self._get_node_duration(node)
 
         # measure instruction handling is bit tricky due to clbit_write_latency
         t0q = max(self._idle_after[q] for q in node.qargs)
-        t0c = max(self._idle_after[c] for c in node.cargs)
-        # Assume following case (t0c > t0q)
-        #
-        #       |t0q
-        # Q ▒▒▒▒░░░░░░░░░░░░
-        # C ▒▒▒▒▒▒▒▒░░░░░░░░
-        #           |t0c
-        #
-        # In this case, there is no actual clbit access until clbit_write_latency.
-        # The node t0 can be push backward by this amount.
-        #
-        #         |t0q' = t0c - clbit_write_latency
-        # Q ▒▒▒▒░░▒▒▒▒▒▒▒▒▒▒
-        # C ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-        #           |t0c' = t0c
-        #
-        # rather than naively doing
-        #
-        #           |t0q' = t0c
-        # Q ▒▒▒▒░░░░▒▒▒▒▒▒▒▒
-        # C ▒▒▒▒▒▒▒▒░░░▒▒▒▒▒
-        #              |t0c' = t0c + clbit_write_latency
-        #
-        t0 = max(t0q, t0c - self._clbit_write_latency)
+        t0 = t0q
         t1 = t0 + op_duration
         for clbit in node.cargs:
             self._idle_after[clbit] = t1
 
-        return t0, t1
+        self._update_idles(node, t0, t1)
 
     def _visit_generic(self, node):
         """Visit a generic node such as a gate or barrier."""
@@ -191,4 +181,10 @@ class DynamicCircuitScheduleAnalysis(BaseScheduler):
         # It happens to be directives such as barrier
         t0 = max(self._idle_after[bit] for bit in node.qargs + node.cargs)
         t1 = t0 + op_duration
-        return t0, t1
+        self._update_idles(node, t0, t1)
+
+    def _update_idles(self, node, t0, t1):
+        for bit in node.qargs:
+            self._idle_after[bit] = t1
+
+        self._node_start_time[node] = t0
