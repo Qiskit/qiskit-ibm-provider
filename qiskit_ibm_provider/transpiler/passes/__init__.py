3c5408d7deb960801b664b0b474feb2f1125b5c2
Thomas Alexander <thomas.a.alexander@ibm.com>
2022-08-24 14:07:13 -0300

    Add dynamic circuits scheduling pass (#365)
    * Fix lint for Qiskit main branch.
    
    * Fix sphinx language configuration.
    
    * Add initial scaffolding for dynamic_circuits module.
    
    * Add a dummy test.
    
    * Add reno.
    
    * Add initial qiskit DynamicalDecoupling scheduling scaffolding and tests.
    
    * Refactor scheduler with visitor pattern.
    
    * Refactor nodes to no longer return. Remove special handling for classical bits.
    
    * Add block based scheduling and updated tests for dynamic circuits type scheduling.
    
    * Refactor padder class.
    
    * Base padding is working.
    
    * Remove debug statements.
    
    * Update non conditional tests to pass.
    
    * c_if scheduling tests are working.
    
    * All tests passing.
    
    * All dynamic circuits tests passing.
    
    * Black formatting.
    
    * Move the dynamic_circuits module to a generic transpiler module.
    
    * Blackify
    
    * Linting.
    
    * Blackify.
    
    * Reach black and pylint fixed point.
    
    * Mypyify the code.
    
    * Add initial scaffolding for dynamic_circuits module.
    
    * Add a dummy test.
    
    * Add reno.
    
    * Add initial qiskit DynamicalDecoupling scheduling scaffolding and tests.
    
    * Refactor scheduler with visitor pattern.
    
    * Refactor nodes to no longer return. Remove special handling for classical bits.
    
    * Add block based scheduling and updated tests for dynamic circuits type scheduling.
    
    * Refactor padder class.
    
    * Base padding is working.
    
    * Remove debug statements.
    
    * Update non conditional tests to pass.
    
    * c_if scheduling tests are working.
    
    * All tests passing.
    
    * All dynamic circuits tests passing.
    
    * Black formatting.
    
    * Move the dynamic_circuits module to a generic transpiler module.
    
    * Blackify
    
    * Linting.
    
    * Blackify.
    
    * Reach black and pylint fixed point.
    
    * Mypyify the code.
    
    * Rename BasePadding to BlockBasePadder.
    
    * Fix broken tests by Qiskit upgrade.
    
    * Move tests into unit.
    
    * linting.
    
    * Update documentation.
    
    * Fix requirements dev for matplotlib drawer./
    
    * Chain qargs and cargs.
    
    * Update pulse block comment.
    
    * Update measurement comment.
    
    * Add t0 comment.
    
    * Move scheduler unit tests.
    
    * Make scheduler idempotent.
    
    * Operations on qubits already measured trigger block termination.
    
    * Black.
    
    * Update comment on _visit_measure qargs.
    
    * Update comment typo.
    
    * Update comment on neighbor ordering.
    
    * Add comments.
    
    Co-authored-by: Rathish Cholarajan <rathishc24@gmail.com>
    Co-authored-by: Kevin Tian <kevin.tian@ibm.com>

---
+++ qiskit_ibm_provider/transpiler/passes/__init__.py
@@ -0,0 +1,30 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""
+================================================================
+Transpiler Passes (:mod:`qiskit_ibm_provider.transpiler.passes`)
+================================================================
+
+.. currentmodule:: qiskit_ibm_provider.transpiler.passes
+
+.. autosummary::
+   :toctree: ../stubs/
+
+   scheduling
+
+
+"""
+
+# circuit scheduling
+from .scheduling import DynamicCircuitScheduleAnalysis
+from .scheduling import PadDelay
