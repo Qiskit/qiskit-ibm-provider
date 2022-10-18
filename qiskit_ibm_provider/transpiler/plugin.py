74bedf1e7337b87599ce69a58065f7915a7822bb
Matthew Treinish <mtreinish@kortar.org>
2022-10-18 13:25:54 -0400

    Add transpiler stage plugin for translating c_if to if_else
    This commit adds a new transpiler stage plugin to the ibm provider which
    is the default basis translator stage with the additional initial step of
    converting c_if conditionals to if_else instructions in the circuit
    prior to performing basis translation.
    
    The next step after this is to add a method
    `get_translation_stage_plugin` to the `IBMBackend` class and have it
    return `"ibm_dynamic_circuits"` when we're targeting a
    dynamic circuits backend [1]. Doing this will mean that by default a user
    calling `transpile()` will automatically use this plugin when targeting
    an appropriate IBMBackend. However, this was not included in the PR
    because it was not clear how to determine the type of backend `self`
    was (in relation to supporting dynamic circuits or not) and I expect
    this is likely something being worked on in #408, so we can add this after we
    determine how we're representing whether a backend object supports
    dynamic circuits or not.
    
    [1] https://qiskit.org/documentation/apidoc/providers.html#custom-transpiler-passes
    
    Starts #418

---
+++ qiskit_ibm_provider/transpiler/plugin.py
@@ -0,0 +1,46 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Plugin for IBM provider backend transpiler stages."""
+
+from typing import Optional
+
+from qiskit.transpiler.passmanager import PassManager
+from qiskit.transpiler.passmanager_config import PassManagerConfig
+from qiskit.transpiler.preset_passmanagers.plugin import PassManagerStagePlugin
+from qiskit.transpiler.preset_passmanagers import common
+from qiskit.transpiler.passes import ConvertConditionsToIfOps
+
+
+class IBMTranslationPlugin(PassManagerStagePlugin):
+    """A translation stage plugin for converting c_if to if_else and then running the basis
+    translator."""
+
+    def pass_manager(
+        self,
+        pass_manager_config: PassManagerConfig,
+        optimization_level: Optional[int] = None
+    ) -> PassManager:
+        """Build IBMTranslationPlugin PassManager."""
+
+        output = PassManager([ConvertConditionsToIfOps()])
+        output += common.generate_translation_passmanager(
+            target=pass_manager_config.target,
+            basis_gates=pass_manager_config.basis_gates,
+            approximation_degree=pass_manager_config.approximation_degree,
+            coupling_map=pass_manager_config.coupling_map,
+            backend_props=pass_manager_config.backend_properties,
+            unitary_synthesis_method=pass_manager_config.unitary_synthesis_method,
+            unitary_synthesis_plugin_config=pass_manager_config.unitary_synthesis_plugin_config,
+            hls_config=pass_manager_config.hls_config
+        )
+        return output
