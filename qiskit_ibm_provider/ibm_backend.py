1723dbba7d7602a46486b590f6f6777e20a69643
Jessie Yu <jessieyu@us.ibm.com>
2022-10-27 10:32:31 -0400

    skip transpilation (#433)

--- qiskit_ibm_provider/ibm_backend.py
+++ qiskit_ibm_provider/ibm_backend.py
@@ -468,6 +468,8 @@ class IBMBackend(Backend):
         )
 
         run_config_dict["circuits"] = circuits
+        # Transpiling in circuit/qasm3 runner is deprecated.
+        run_config_dict["skip_transpilation"] = True
 
         return self._runtime_run(
             program_id=program_id,
