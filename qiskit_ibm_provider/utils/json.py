8ede0c65492406289b5f9f7f1c5c5fff3b8feac7
Julien Gacon <gaconju@gmail.com>
2023-03-29 14:13:09 +0200

    remove decompose of blueprint in json encode

--- qiskit_ibm_provider/utils/json.py
+++ qiskit_ibm_provider/utils/json.py
@@ -211,9 +211,6 @@ class RuntimeEncoder(json.JSONEncoder):
         if hasattr(obj, "to_json"):
             return {"__type__": "to_json", "__value__": obj.to_json()}
         if isinstance(obj, QuantumCircuit):
-            # TODO Remove the decompose when terra 6713 is released.
-            if isinstance(obj, BlueprintCircuit):
-                obj = obj.decompose()
             value = _serialize_and_encode(
                 data=obj,
                 serializer=lambda buff, data: dump(data, buff),  # type: ignore[no-untyped-call]
