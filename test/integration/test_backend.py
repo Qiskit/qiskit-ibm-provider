2b587a5732ffe2c9e204d1cef0b208c56d557f56
merav-aharoni <merav@il.ibm.com>
2023-05-09 09:05:30 +0000

    Moved test to integration and fixed it

--- test/integration/test_backend.py
+++ test/integration/test_backend.py
@@ -19,9 +19,12 @@ from qiskit import QuantumCircuit, transpile
 from qiskit.providers.models import QasmBackendConfiguration
 from qiskit.providers.exceptions import QiskitBackendNotFoundError
 from qiskit.test.reference_circuits import ReferenceCircuits
+from qiskit.pulse import Schedule
 
 from qiskit_ibm_provider import IBMBackend, IBMProvider
 from qiskit_ibm_provider.ibm_qubit_properties import IBMQubitProperties
+from qiskit_ibm_provider.exceptions import IBMBackendValueError
+
 from ..decorators import (
     IntegrationTestDependencies,
     integration_test_setup_with_backend,
@@ -194,3 +197,15 @@ class TestIBMBackend(IBMTestCase):
         """Test that an error is raised when retrieving a backend that does not exist."""
         with self.assertRaises(QiskitBackendNotFoundError):
             self.dependencies.provider.get_backend("nonexistent_backend")
+
+    def test_schedule_error_message(self):
+        """Test that passing a Schedule as input to Backend.run() raises an error."""
+        backend = self.dependencies.provider.get_backend("ibmq_qasm_simulator")
+        schedule = Schedule()
+        for circuit in [schedule, [schedule]]:
+            with self.assertRaises(IBMBackendValueError) as err:
+                backend.run(circuit)
+            self.assertIn(
+                "Class 'Schedule' is no longer supported as an input circuit",
+                str(err.exception),
+            )
