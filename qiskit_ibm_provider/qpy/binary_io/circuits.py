0766f54c405afd9ac0cf29b1de28a497692d7140
kevin-tian <kevin.tian@ibm.com>
2023-09-21 15:22:44 -0400

    Update qpy qiskit 0.45

--- qiskit_ibm_provider/qpy/binary_io/circuits.py
+++ qiskit_ibm_provider/qpy/binary_io/circuits.py
@@ -25,10 +25,11 @@ import numpy as np
 
 from qiskit import circuit as circuit_mod
 from qiskit import extensions
-from qiskit.circuit import library, controlflow, CircuitInstruction
+from qiskit.circuit import library, controlflow, CircuitInstruction, ControlFlowOp
 from qiskit.circuit.classical import expr
 from qiskit.circuit.classicalregister import ClassicalRegister, Clbit
 from qiskit.circuit.gate import Gate
+from qiskit.circuit.singleton_gate import SingletonGate
 from qiskit.circuit.controlledgate import ControlledGate
 from qiskit.circuit.instruction import Instruction
 from qiskit.circuit.quantumcircuit import QuantumCircuit
@@ -289,8 +290,10 @@ def _read_instruction(  # type: ignore[no-untyped-def]
     else:
         raise AttributeError("Invalid instruction type: %s" % gate_name)
 
+    if instruction.label_size <= 0:
+        label = None
     if gate_name in {"IfElseOp", "WhileLoopOp"}:
-        gate = gate_class(condition, *params)
+        gate = gate_class(condition, *params, label=label)
     elif version >= 5 and issubclass(gate_class, ControlledGate):
         if gate_name in {
             "MCPhaseGate",
@@ -300,9 +303,9 @@ def _read_instruction(  # type: ignore[no-untyped-def]
             "MCXRecursive",
             "MCXVChain",
         }:
-            gate = gate_class(*params, instruction.num_ctrl_qubits)
+            gate = gate_class(*params, instruction.num_ctrl_qubits, label=label)
         else:
-            gate = gate_class(*params)
+            gate = gate_class(*params, label=label)
             gate.num_ctrl_qubits = instruction.num_ctrl_qubits
             gate.ctrl_state = instruction.ctrl_state
         gate.condition = condition
@@ -321,10 +324,19 @@ def _read_instruction(  # type: ignore[no-untyped-def]
                 params = [len(qargs)]
             elif gate_name in {"BreakLoopOp", "ContinueLoopOp"}:
                 params = [len(qargs), len(cargs)]
-            gate = gate_class(*params)
-        gate.condition = condition
-    if instruction.label_size > 0:
-        gate.label = label
+            if label is not None:
+                if issubclass(gate_class, SingletonGate):
+                    gate = gate_class(*params, label=label)
+                else:
+                    gate = gate_class(*params)
+                    gate.label = label
+            else:
+                gate = gate_class(*params)
+        if condition:
+            if not isinstance(gate, ControlFlowOp):
+                gate = gate.c_if(*condition)
+            else:
+                gate.condition = condition
     if circuit is None:
         return gate
     if not isinstance(gate, Instruction):
@@ -580,13 +592,17 @@ def _write_instruction(  # type: ignore[no-untyped-def]
         )
         or gate_class_name == "Gate"
         or gate_class_name == "Instruction"
-        or gate_class_name == "ControlledGate"
         or isinstance(instruction.operation, library.BlueprintCircuit)
     ):
         if instruction.operation.name not in custom_operations:
             custom_operations[instruction.operation.name] = instruction.operation
             custom_operations_list.append(instruction.operation.name)
         gate_class_name = instruction.operation.name
+
+    elif gate_class_name == "ControlledGate":
+        gate_class_name = instruction.operation.name + "_" + str(uuid.uuid4())
+        custom_operations[gate_class_name] = instruction.operation
+        custom_operations_list.append(gate_class_name)
 
     elif isinstance(instruction.operation, library.PauliEvolutionGate):
         gate_class_name = r"###PauliEvolutionGate_" + str(uuid.uuid4())
