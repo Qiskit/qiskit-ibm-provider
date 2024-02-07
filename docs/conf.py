a1a202fdd2a2694b282405cf01ea93db9075e661
kevin-tian <kevin.tian@ibm.com>
2024-02-07 11:22:18 -0500

    Prepare release 0.9.0

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ author = 'Qiskit Development Team'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.8.1'
+release = '0.9.0'
 
 # -- General configuration ---------------------------------------------------
 
